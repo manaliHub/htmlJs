8352df39d85f635eebd6bb95d0725b2f6ea4dac5
manaliHub <moholkar.manali@gmail.com>
2020-12-22 11:53:41 +1100

    3 files modified

--- index.js
+++ index.js
@@ -4,3 +4,17 @@ import './style.css';
 // Write Javascript code!
 const appDiv = document.getElementById('app');
 appDiv.innerHTML = `<h1>JS Starter</h1>`;
+
+
+function clicked(){
+	
+	var name=document.getElementById("nameText");
+	name.value="Aditya";
+	name.setAttribute("style","color:red");
+	
+}
+
+function sumFunction(num1,num2){
+	var sum = parseInt(num1)+parseInt(num2);
+	return sum;
+}
